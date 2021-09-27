7277511dd1b3790b30ecfae28c8c8bf6a05102c1
Gabriel <gfranklings@gmail.com>
2021-09-26 22:04:27 -0400

    alacritty: changed font size to 11 & alacritty: changed font (Mononoki NF -> Sauce Code Pro Nerd Font) & added qutebrowser config

---
+++ .local/bin/app.py
@@ -0,0 +1,21 @@
+import curses
+import os
+
+def main(win):
+    win.nodelay(True)
+    key=""
+    win.clear()                
+    win.addstr("Detected key:")
+    while 1:          
+        try:                 
+           key = win.getkey()         
+           win.clear()                
+           win.addstr("Detected key:")
+           win.addstr(str(key)) 
+           if key == os.linesep:
+              break           
+        except Exception as e:
+           # No input   
+           pass         
+
+curses.wrapper(main)
