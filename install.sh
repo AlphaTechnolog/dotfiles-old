ca2d39a77a5aadc2ef5db89fcd1cb74f184b8d21
Gabriel <gfranklings@gmail.com>
2021-10-10 10:35:52 -0400

    install.sh: Set 26.png

--- install.sh
+++ install.sh
@@ -500,9 +500,9 @@ setup_wallpaper() {
   echo "==> /usr/local/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers"
   prevented_process /usr/local/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers
 
-  if confirm "Do you want to setup a default wl wallpaper 50.jpeg? " 1; then
-    echo "==> /usr/local/bin/wl set 49.jpeg"
-    prevented_process /usr/local/bin/wl set 49.jpeg
+  if confirm "Do you want to setup a default wl wallpaper 26.png? " 1; then
+    echo "==> /usr/local/bin/wl set 26.png"
+    prevented_process /usr/local/bin/wl set 26.png
   fi
 
   press_enter_to_continue
