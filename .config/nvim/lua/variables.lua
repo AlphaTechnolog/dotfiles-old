cd3e9573e94e92825617dd66f1c3546978c6df68
Gabriel <gfranklings@gmail.com>
2021-10-15 22:14:00 -0400

    added some wallpapers & fixed some vim problems

--- .config/nvim/lua/variables.lua
+++ .config/nvim/lua/variables.lua
@@ -1,23 +1,23 @@
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
+vim.bo.expandtab = true
 vim.o.number = true
 vim.o.wrap = false
 vim.o.clipboard = 'unnamedplus'
 vim.o.showmode = false
 vim.o.mouse = 'a'
 
-vim.cmd [[ autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 ]]
-vim.cmd [[ autocmd FileType c setlocal tabstop=4 shfitwidth=4 softtabstop=4 ]]
-vim.cmd [[ autocmd FileType c++ setlocal tabstop=4 shiftwidth=4 softtabstop=4 ]]
-vim.cmd [[ autocmd FileType php setlocal tabstop=4 shiftwidth=4 softtabstop=4 ]]
-vim.cmd [[ autocmd FileType lua setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType javascriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType html setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType css setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
-vim.cmd [[ autocmd FileType vue setlocal tabstop=2 shiftwidth=2 softtabstop=2 ]]
+vim.cmd [[ autocmd FileType python setlocal tabstop=4 shiftwidth=4 expandtab ]]
+vim.cmd [[ autocmd FileType c setlocal tabstop=4 shfitwidth=4 expandtab ]]
+vim.cmd [[ autocmd FileType c++ setlocal tabstop=4 shiftwidth=4 expandtab ]]
+vim.cmd [[ autocmd FileType php setlocal tabstop=4 shiftwidth=4 expandtab ]]
+vim.cmd [[ autocmd FileType lua setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType javascriptreact setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType html setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType css setlocal tabstop=2 shiftwidth=2 expandtab ]]
+vim.cmd [[ autocmd FileType vue setlocal tabstop=2 shiftwidth=2 expandtab ]]
 
 vim.cmd [[ set shell=/bin/bash ]]
