437d1ef3c1eb935ce95dcf64bf2292b03ea478f4
Gabriel <gfranklings@gmail.com>
2021-10-16 09:58:33 -0400

    install.sh: Switched to 35.jpeg

--- install.sh
+++ install.sh
@@ -492,9 +492,9 @@ setup_wallpaper() {
   echo "==> /usr/local/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers"
   prevented_process /usr/local/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers
 
-  if confirm "Do you want to setup a default wl wallpaper 61.jpeg? " 1; then
-    echo "==> /usr/local/bin/wl set 61.jpeg"
-    prevented_process /usr/local/bin/wl set 61.jpeg
+  if confirm "Do you want to setup a default wl wallpaper 35.jpeg? " 1; then
+    echo "==> /usr/local/bin/wl set 35.jpeg"
+    prevented_process /usr/local/bin/wl set 35.jpeg
   fi
 
   press_enter_to_continue
