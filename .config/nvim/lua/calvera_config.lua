f3eb9df6149030e63e3136f22504c60836c04830
Gabriel <gfranklings@gmail.com>
2021-10-18 08:00:16 -0400

    some calvera dark changes

--- .config/nvim/lua/calvera_config.lua
+++ .config/nvim/lua/calvera_config.lua
@@ -1,7 +1,7 @@
 vim.g.calvera_italic_comments = false
 vim.g.calvera_italic_keywords = false
-vim.g.calvera_borders = false
-vim.g.calvera_contrast = false
+vim.g.calvera_borders = true
+vim.g.calvera_contrast = true
 vim.g.calvera_hide_eob = true
 
 require('calvera').set()
