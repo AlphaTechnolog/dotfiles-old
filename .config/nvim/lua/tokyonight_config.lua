ed307efaf2b4d8ad051c35cf5d52306788930650
Gabriel <gfranklings@gmail.com>
2021-10-18 14:19:35 -0400

    nvim: tokyonight.lua: disabled italics

--- .config/nvim/lua/tokyonight_config.lua
+++ .config/nvim/lua/tokyonight_config.lua
@@ -1,8 +1,8 @@
 vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_italic_keywords = true
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_variables = true
+vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_keywords = false
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_italic_variables = false
 vim.g.tokyonight_hide_inactive_statusline = true
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
