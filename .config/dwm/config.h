1b0b96b822c4d89a6fdaefe83f8fb74bf2c2f6e8
Gabriel Guerra <gfranklings@gmail.com>
2021-11-21 09:34:18 -0400

    dwm: uncommented all autostart.sh

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,7 +1,7 @@
 // -------------------------------- Appearance ---------------------------------
 
 // height of bar
-static const int user_bh = 27;
+static const int user_bh = 25;
 // border pixel of windows
 static const unsigned int borderpx  = 0;
 // gaps between windows 
