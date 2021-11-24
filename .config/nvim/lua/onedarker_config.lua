0d01ea5dd602cdd45f8d2918a3d34ad1c33fa7d9
Gabriel Guerra <gfranklings@gmail.com>
2021-11-24 12:52:23 -0400

    some opacity changes

--- .config/nvim/lua/onedarker_config.lua
+++ .config/nvim/lua/onedarker_config.lua
@@ -1,7 +1,7 @@
-vim.g.onedarker_italic_comments = true
-vim.g.onedarker_italic_keywords = true
-vim.g.onedarker_italic_functions = true
-vim.g.onedarker_italic_variables = true
+vim.g.onedarker_italic_comments = false
+vim.g.onedarker_italic_keywords = false
+vim.g.onedarker_italic_functions = false
+vim.g.onedarker_italic_variables = false
 
 require'onedarker.evil_lualine'
 require'onedarker'
