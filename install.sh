f490307755ab51b38c5f5c619bb3ee3276eed4b8
Gabriel Guerra <gfranklings@gmail.com>
2021-11-26 14:20:07 -0400

    install.sh: Set 67.jpeg as default wallpaper

--- install.sh
+++ install.sh
@@ -492,9 +492,9 @@ setup_wallpaper() {
   echo "==> /usr/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers"
   prevented_process /usr/bin/wl config -k wallpapers_folder -v $dotfiles_path/wallpapers
 
-  if confirm "Do you want to setup a default wl wallpaper 45.jpeg? " 1; then
-    echo "==> /usr/bin/wl set 45.jpeg"
-    prevented_process /usr/bin/wl set 45.jpeg
+  if confirm "Do you want to setup a default wl wallpaper 67.jpeg? " 1; then
+    echo "==> /usr/bin/wl set 67.jpeg"
+    prevented_process /usr/bin/wl set 67.jpeg
   fi
 
   press_enter_to_continue
