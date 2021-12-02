5d8aaf7a63d6d863b24302930c5b993beb1ccbf6
Gabriel Guerra <gfranklings@gmail.com>
2021-12-02 12:44:08 -0400

    spectrwm: baraction: added the update widget

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -13,21 +13,16 @@ mem() {
   echo -e "$mem"
 }
 
-## CPU
-cpu() {
-  read cpu a b c previdle rest < /proc/stat
-  prevtotal=$((a+b+c+previdle))
-  sleep 0.5
-  read cpu a b c idle rest < /proc/stat
-  total=$((a+b+c+idle))
-  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  echo -e "$cpu%"
-}
-
 ## VOLUME
 vol() {
     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
     echo -e "$vol"
+}
+
+## Updates
+updates () {
+  updates=$(checkupdates | wc -l)
+  echo -e "$updates"
 }
 
 SLEEP_SEC=3
@@ -38,6 +33,6 @@ SLEEP_SEC=3
 # So I would love to add more functions to this script but it makes the 
 # echo output too long to display correctly.
 while :; do
-  echo "+@fg=1; +@fn=0; $(cpu) +@fg=0; +@fg=2;  +@fn=0; $(mem) +@fg=0; +@fg=3;  +@fn=0; $(hdd) +@fg=0; +@fg=4; 墳 +@fn=0; $(vol) +@fg=0;"
+  echo "+@fg=1; +@fn=0; $(updates) +@fg=0; +@fg=2;  +@fn=0; $(mem) +@fg=0; +@fg=3;  +@fn=0; $(hdd) +@fg=0; +@fg=4; 墳 +@fn=0; $(vol) +@fg=0;"
 	sleep $SLEEP_SEC
 done
