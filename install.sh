a2b00ea5539f5289cf696edf9ffd71acb650a689
Gabriel Guerra <gfranklings@gmail.com>
2021-12-11 17:53:04 -0400

    install.sh: Set 04.png as default wallpaper

--- install.sh
+++ install.sh
@@ -551,8 +551,8 @@ setup_wallpaper() {
 
   wl_cmd config -k wallpapers_folder -v $dotfiles_path/wallpapers
 
-  if confirm "Do you want to setup a default wl wallpaper (73.jpeg)? " 1; then
-    wl_cmd set 73.jpeg
+  if confirm "Do you want to setup a default wl wallpaper (04.png)? " 1; then
+    wl_cmd set 04.png
   fi
 
   press_enter_to_continue
